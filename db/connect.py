681e7106bc51c510a2cf7e06a40907a512f2cc4a
Lu Daven <daven.l@triplet-rd.com>
2021-10-29 08:47:53 +0900

    connect

--- db/connect.py
+++ db/connect.py
@@ -1,53 +1,53 @@
 #!/usr/bin/python
-# import psycopg2
-# from config import config
+import psycopg2
+from config import config
 
-# def connect():
-#     """ Connect to the PostgreSQL database server """
-#     conn = None
-#     try:
-#         # read connection parameters
-#         params = config()
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
 
-#         # connect to the PostgreSQL server
-#         print('Connecting to the PostgreSQL database...')
-#         conn = psycopg2.connect(**params)
-#         print(conn)
-#         print(type(conn))
-#         # create a cursor
-#         cur = conn.cursor()
-#         print(cur)
-#         print(type(cur))
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+        print(conn)
+        print(type(conn))
+        # create a cursor
+        cur = conn.cursor()
+        print(cur)
+        print(type(cur))
         
-# 	# execute a statement
-#         print('PostgreSQL database version:')
-#         cur.execute('SELECT version()')
+	# execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
 
-#         # display the PostgreSQL database server version
-#         db_version = cur.fetchone()
-#         print(db_version)
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
        
-#         print('Creating DB contents...')
-#         cur.execute("CREATE TABLE test_db (id serial PRIMARY KEY, num integer, data varchar);")
+        print('Creating DB contents...')
+        cur.execute("CREATE TABLE test_db (id serial PRIMARY KEY, num integer, data varchar);")
 
-#         print("Select table that I made")
-#         cur.execute("INSERT INTO test_db (num, data) VALUES (%s, %s)",(100, "abc'def"))
+        print("Select table that I made")
+        cur.execute("INSERT INTO test_db (num, data) VALUES (%s, %s)",(100, "abc'def"))
 
-#         print("Test to see if DB retrieves it")
-#         cur.execute("SELECT * FROM test_db;")
+        print("Test to see if DB retrieves it")
+        cur.execute("SELECT * FROM test_db;")
 
-#         fetched_data = cur.fetchone()
-#         print(fetched_data)
-# 	# close the communication with the PostgreSQL
-#         cur.close()
+        fetched_data = cur.fetchone()
+        print(fetched_data)
+	# close the communication with the PostgreSQL
+        cur.close()
 
-#     except (Exception, psycopg2.DatabaseError) as error:
-#         print(error)
-#     finally:
-#         if conn is not None:
-#             conn.close()
-#             print('Database connection closed.')
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
 
 
-# if __name__ == '__main__':
-#     connect()
+if __name__ == '__main__':
+    connect()
