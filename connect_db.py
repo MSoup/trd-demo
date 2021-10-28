eb1315c6f48a18cb00304ba9592ada744fd84bfc
Lu Daven <daven.l@triplet-rd.com>
2021-10-28 14:24:49 +0900

    create test db

--- connect_db.py
+++ connect_db.py
@@ -2,14 +2,16 @@
 # Small script to show PostgreSQL and Pyscopg together
 #
 
+# host='localhost'
+
 import psycopg2
 
 try:
-    conn = psycopg2.connect("dbname='test' user='postgres' host='localhost' password='dbpass'")
+    con = psycopg2.connect("dbname='github_actions' user='postgres' password='dbpass'")
 except:
     print("I am unable to connect to the database")
 
 
-cur = conn.cursor()
+cur = con.cursor()
 
 cur.execute("CREATE DATABASE test")
