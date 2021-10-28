6f0d61f2c84f1c302bb3edea1a860d0d8ec947f4
Lu Daven <daven.l@triplet-rd.com>
2021-10-28 16:43:05 +0900

    bravely using method to insert into my table

--- db/connect.py
+++ db/connect.py
@@ -25,15 +25,14 @@ def connect():
         print(db_version)
        
         print('Creating DB contents...')
-        command = """(
-            part_id serial PRIMARY KEY,
-            name VARCHAR ( 50 ) NOT NULL,
-            created_on TIMESTAMP NOT NULL);
-        """
-        cur.execute(command)
+        cur.execute("CREATE TABLE test_table (id serial PRIMARY KEY, num integer, data varchar);")
+
+        print("Trying to select table I just made")
+        cur.execute(f"INSERT INTO test_db (num, data) VALUES {100}, {"abc'def"}")
 
 	# close the communication with the PostgreSQL
         cur.close()
+        
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
