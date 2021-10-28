58c9e02270d68d4959d0f4024842231c981ba396
Lu Daven <daven.l@triplet-rd.com>
2021-10-28 14:47:32 +0900

    oops I forgot to add dependencies

--- connect_db.py
+++ connect_db.py
@@ -1,17 +1,13 @@
-#
 # Small script to show PostgreSQL and Pyscopg together
 #
 
-# host='localhost'
+# 
 
 import psycopg2
 
 try:
-    con = psycopg2.connect("dbname='github_actions' user='postgres' password='dbpass'")
+    con = psycopg2.connect("dbname='github_actions' host='localhost' user='postgres' password='dbpass'")
+    cur = con.cursor()
+    cur.execute("CREATE DATABASE test")
 except:
     print("I am unable to connect to the database")
-
-
-cur = con.cursor()
-
-cur.execute("CREATE DATABASE test")
